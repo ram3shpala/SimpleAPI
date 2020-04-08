46c3f9d85b11cd2d6bc75e5fbe7713e89fb931e1
Ramesh Pala <ramesh.pala@colruytgroup.com>
2020-04-08 11:54:30 +0200

    Initial commit

---
+++ src/main/java/com/demo/openshift/SimpleAPI/SimpleApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.openshift.SimpleAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiApplication.class, args);
+	}
+
+}
